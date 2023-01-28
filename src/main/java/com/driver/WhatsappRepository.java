72022f932a90070b62e91ff1aa6e6dd2c720f4e0
TarunVanshkar <tarunben33@gmail.com>
2023-01-28 17:35:51 +0530

    Third_commit

--- src/main/java/com/driver/WhatsappRepository.java
+++ src/main/java/com/driver/WhatsappRepository.java
@@ -96,6 +96,11 @@ public class WhatsappRepository {
 
     public String changeAdmin(User approver, User user, Group group)
     {
+        //Throw "Group does not exist" if the mentioned group does not exist
+        //Throw "Approver does not have rights" if the approver is not the current admin of the group
+        //Throw "User is not a participant" if the user is not a part of the group
+        //Change the admin of the group to "user" and return "SUCCESS". Note that at one time there is only one admin and the admin rights are transferred from approver to user.
+
         //private HashMap<Group, User> adminMap;
         if(!adminMap.containsKey(group))
         {
